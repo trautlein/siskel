f5ab434ec166b028b7f0765af07fd50f6cf58c0a
trautlein <hans@trautlein.com>
2016-09-28 16:50:41 -0700

    Movie View re-renders when the model changes

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -62,7 +62,7 @@ var MovieView = Backbone.View.extend({
                         </div>'),
 
   initialize: function() {
-    // your code here
+    this.model.on('change', this.render, this);
   },
 
   events: {
